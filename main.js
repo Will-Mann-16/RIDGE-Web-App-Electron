db111a5429ab4b9777fb831ac9feb5a687a6d862
Will Mann <william@mannclan.net>
2018-06-03 06:39:28 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,51 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, protocol} = require('electron')
+const url = require('url')
+const path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let window
+
+function createWindow () {
+  window = new BrowserWindow({ width: 1280, height: 720, autoHideMenuBar: true, useContentSize: true, icon: "./favicon.png" });
+  window.loadURL(url.format({
+    pathname: 'index.html',    /* Attention here: origin is path.join(__dirname, 'index.html') */
+    protocol: 'file',
+    slashes: true
+  }))
+
+  window.on('closed', () => {
+    window = null
+  })
+}
+
+app.on('ready', () => {
+  protocol.interceptFileProtocol('file', (request, callback) => {
+    const url = request.url.substr(7)    /* all urls start with 'file://' */
+    callback({ path: path.normalize(`${__dirname}/${url}`)})
+  }, (err) => {
+    if (err) console.error('Failed to register protocol')
+  })
+  createWindow()
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (window === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
